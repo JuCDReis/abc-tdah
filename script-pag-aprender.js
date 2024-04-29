c236ea19eca3ee7f5f7fc813ba4b2932634110cf
Julia Reis <julia.reisfreela@gmail.com>
2024-04-29 10:31:55 -0300

    add script-pag-aprendendo

---
+++ script-pag-aprender.js
@@ -0,0 +1,36 @@
+const qualAtv = document.querySelector(".main-container").getAttribute("id");
+const itens = document.querySelectorAll('.option');
+
+storageAtv();
+
+
+function storageAtv() {
+    localStorage.removeItem('qualAtv');
+    switch (qualAtv) {
+        case "abc":
+            localStorage.setItem('qualAtv', 'abc');
+            break;
+        case "num":
+            localStorage.setItem('qualAtv', 'num');
+            break;
+        case "desenhos":
+            localStorage.setItem('qualAtv', 'desenhos');
+            break;
+        default:
+        console.error();
+            break;
+    }
+}
+
+function storageItem(item) {
+    localStorage.removeItem('item');
+    localStorage.setItem('item', item);
+}
+
+for (const item of itens) {
+    item.addEventListener("click", () => {
+        const itemId = item.getAttribute('id');
+        storageItem(itemId);
+        window.location.href = "./atv.html";
+    })
+}
